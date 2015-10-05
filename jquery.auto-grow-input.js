032f0db6c9964da3bf09f7ee89a5e7df8da6278f
Dan <gravyzzap@gmail.com>
2015-10-05 20:49:11 +0300

    Fix always false condition

--- jquery.auto-grow-input.js
+++ jquery.auto-grow-input.js
@@ -29,7 +29,7 @@
                     ariaHidden: true
                 }).appendTo('body'),
                 check = function(e){
-                    if (val === (val = input.val()) && !e.type == 'autogrow') return;
+                    if (val === (val = input.val()) && e.type !== 'autogrow') return;
                     if (!val) val = input.attr('placeholder') || '';
                     span.html(val.replace(/&/g, '&amp;').replace(/\s/g, '&nbsp;').replace(/</g, '&lt;').replace(/>/g, '&gt;'));
                     var newWidth = span.width() + comfortZone, mw = typeof(o.maxWidth) == "function" ? o.maxWidth() : o.maxWidth;
